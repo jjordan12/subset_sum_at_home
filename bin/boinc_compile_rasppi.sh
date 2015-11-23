9dc07f18c1322ccb54daa43e6d895324bdb74754
Joe <harwy.pikul@gmail.com>
2015-11-23 14:27:59 +0000

    added compile script for the mpi program

---
+++ bin/boinc_compile_rasppi.sh
@@ -0,0 +1,14 @@
+#compile BOINC with:
+
+#_autosetup
+#./configure --disable-client --disable-server LDFLAGS=-static-libgcc
+#make
+
+#ln -s `g++ -print-file-name=libstdc++.a`
+#make
+
+
+
+cd ../client/
+mpic++ -DVERSION=$1 -DVERBOSE -DENABLE_CHECKPOINTING -DFALSE_ONLY -D_BOINC_ -D__STDC_LIMIT_MACROS -Ofast -mfpu=neon-vfpv4 -mfloat-abi=hard -ftree-vectorize -funroll-loops -Wall -I/home/pi/boinc -I/home/pi/boinc/api -I/home/pi/boinc/lib subset_sum_main.cpp ../common/binary_output.cpp ../common/generate_subsets.cpp ../common/n_choose_k.cpp -o ../bin/SubsetSum_$1_arm-unknown-linux-gnueabihf -L/home/pi/boinc/lib -L/home/pi/boinc/api -lboinc_api -lboinc -pthread
+cd ../bin/
